1ac37abf254c9bf773a788770a65dc682bddcaf9
kajawetzel <167359659+kajawetzel@users.noreply.github.com>
2025-01-07 18:50:02 +0100

    add eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+module.exports = function (eleventyConfig) {
+  
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("./posts/*.md");
+  });
+
+  eleventyConfig.addPassthroughCopy("assets");
+
+
+
+eleventyConfig.addFilter("addTargetBlank", function(content) {
+    const { JSDOM } = require("jsdom");
+    const dom = new JSDOM(content);
+    const links = dom.window.document.querySelectorAll("a");
+
+    links.forEach(link => {
+      link.setAttribute("target", "_blank");
+      link.setAttribute("rel", "noopener noreferrer");
+    });
+
+    return dom.serialize();
+  });
+};
+
+
+
+
